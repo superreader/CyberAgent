810c15d4a2dd12644cf2d09dfaa239e5801b5a87
shota0829 <shota.kashiwagi.0829@gmail.com>
2012-09-11 10:45:23 +0900

    storybord変更

---
+++ CyberAgent/Result100ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Result100ViewController.h
+//  CyberAgent
+//
+//  Created by 柏木 翔太 on 12/09/11.
+//  Copyright (c) 2012年 上野 博司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Result100ViewController : UIViewController
+
+@end
---
+++ CyberAgent/Result100ViewController.m
@@ -0,0 +1,43 @@
+//
+//  Result100ViewController.m
+//  CyberAgent
+//
+//  Created by 柏木 翔太 on 12/09/11.
+//  Copyright (c) 2012年 上野 博司. All rights reserved.
+//
+
+#import "Result100ViewController.h"
+
+@interface Result100ViewController ()
+
+@end
+
+@implementation Result100ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
